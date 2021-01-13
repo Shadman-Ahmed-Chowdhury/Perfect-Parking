4508d43e4bec89f19032bc9b45503e0d13d986d7
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2021-01-14 01:16:28 +0600

    Add authentication for private routes

--- src/components/BookParking.js
+++ src/components/BookParking.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SweetAlert from "react-bootstrap-sweetalert";
+import authListener from "../app-logic/authListener";
 
 const BookParking = () => {
-  return (
-    <div>
-      <h2>Book Parking</h2>
-    </div>
-  );
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    authListener().onAuthStateChanged((user) => {
+      setUser(user);
+      // if (user !== null) {
+      //   console.log(user.email);
+      // } else {
+      //   console.log("Logged Out");
+      // }
+    });
+  }, []);
+  const goToLogin = () => {
+    window.location.assign("/login");
+  };
+
+  const goToHome = () => {
+    window.location.assign("/");
+  };
+
+  if (user !== null) {
+    // If the user is logged In
+    return (
+      <div>
+        <h2> Book Parking </h2>
+      </div>
+    );
+  } else {
+    // If the user isn't logged in
+    return (
+      <SweetAlert
+        warning
+        confirmBtnText="Login Now!"
+        confirmBtnBsStyle="primary"
+        title="You are not authenticated!"
+        onConfirm={() => goToLogin()}
+        onCancel={() => goToHome()}
+        focusCancelBtn
+      ></SweetAlert>
+    );
+  }
 };
 
 export default BookParking;
